0f5e6e08c235764448f58a1add0a3604f5cbe109
christiano0407 <placebo0704@gmail.com>
2023-11-03 09:43:10 -0600

    add new content in webserver

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 ## Request API FastAPI ##
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
 # App Server #
 app = FastAPI()
 
-@app.get('/')
-def get_home():
-  return [4, 5, 7, 10]
+@app.get('/', response_class=HTMLResponse)
+async def get_home():
+   html_content =  """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1>Hello Dev! HTML & Python!</h1>
+            <h2>Python Backend </h2>
+        </body>
+    </html>
+    """
+   return HTMLResponse(content=html_content, status_code=200)
+
+##HomePage
+@app.get('/home', response_class=HTMLResponse)
+async def get_page(): 
+   html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1>Home Page</h1>
+            <p>Hello World! Python In Backend!!! Amazing</p>
+        </body>
+    </html>
+   """
+   return HTMLResponse(content=html_content, status_code=200)
