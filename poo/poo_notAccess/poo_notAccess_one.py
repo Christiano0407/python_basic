a5b0cffc2d5afc6de0a1b39e219d33e267ce2f70
christiano0407 <placebo0704@gmail.com>
2023-11-07 12:55:46 -0600

    add getters and setters

--- poo/poo_notAccess/poo_notAccess_one.py
+++ poo/poo_notAccess/poo_notAccess_one.py
@@ -1,7 +1,22 @@
-## POO : Not Access Two ##
+## POO : Not Access Two & Herencia ##
 ### Préstamo en un sistema bancario ##
-class Pay:
-  def __init__(self, money, interest_rate, time_pay, balance):
+
+class approved_pay:
+  def __init__(self, approved_money):
+    self._approved_money = approved_money
+
+    #Getters
+  def get_approved(self): 
+    return self._approved_money
+    #Setters
+  def set_approved(self, approved_money): 
+    self._approved_money = approved_money
+
+
+
+class Pay(approved_pay):
+  def __init__(self, money, interest_rate, time_pay, balance, approved_money):
+    super().__init__(approved_money)
     self.money = money # Monto del préstamo
     self.interest_rate = interest_rate #Interéses
     self.time_pay = time_pay #Tiempo a pagar
@@ -25,13 +40,16 @@ class Pay:
   #Private
   def first_pay(self):
     return  self.calculate_pay_month() + self._calculate_iva()
+  #Getters & Setters
+  def set_approved(self, approved_money):
+    return super().set_approved(approved_money)
 
 # Variables = Users # 
-user_credit = Pay(2000, 0.08, 12, 6000)
-user_bank = Pay(8000, 0.10, 12, 10000)
-user_one = Pay(15000, 0.15, 18, 5000)
-user_two = Pay(35000, 0.20, 24, 30000)
-user_three = Pay(50000, 0.25, 36, 10000)
+user_credit = Pay(2000, 0.08, 12, 6000, 2500)
+user_bank = Pay(8000, 0.10, 12, 10000, 2500)
+user_one = Pay(15000, 0.15, 18, 5000, 2500)
+user_two = Pay(35000, 0.20, 24, 30000, 2500)
+user_three = Pay(50000, 0.25, 36, 10000, 2500)
 # Call Pay #
 if __name__ == "__main__": 
   user = f"Fred: Monto de su préstamo: {user_one.money:.2f} y pago total: {user_one._calculate_total_pay():.2f}, por mes pagaría: {user_one.calculate_pay_month():.2f} y los interéses del préstamo: {user_one._calculate_interest_rate():.2f}"
@@ -43,7 +61,7 @@ if __name__ == "__main__":
   #print(f"Interéses del préstamo: {user_credit._calculate_interest_rate():.2f}")
   print(user)
   print(user_house)
-
+  print(user_credit.set_approved(25000))
 
 
     
