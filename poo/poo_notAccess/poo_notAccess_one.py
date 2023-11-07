00373debfc4ccb8a6d257295a593b77275990aec
christiano0407 <placebo0704@gmail.com>
2023-11-07 12:20:39 -0600

    add POO methods

--- poo/poo_notAccess/poo_notAccess_one.py
+++ poo/poo_notAccess/poo_notAccess_one.py
@@ -1,11 +1,11 @@
 ## POO : Not Access Two ##
-
+### Préstamo en un sistema bancario ##
 class Pay:
   def __init__(self, money, interest_rate, time_pay, balance):
-    self.money = money
-    self.interest_rate = interest_rate
-    self.time_pay = time_pay
-    self.__balance = balance #Private (__)
+    self.money = money # Monto del préstamo
+    self.interest_rate = interest_rate #Interéses
+    self.time_pay = time_pay #Tiempo a pagar
+    self.__balance = balance #Private (__) / Saldo
 
   #Access Public #Protected (_)
   def calculate_pay_month(self): 
@@ -14,23 +14,36 @@ class Pay:
   def _calculate_total_pay(self): 
     return self.calculate_pay_month() * self.time_pay
   #Private & Protected
-  def _private_interest_rate(self): 
+  def _calculate_interest_rate(self): 
     return self.money * self.interest_rate * self.time_pay
+  #Credit User
   def user_credit(self): 
     return f"User pay for month: {self.__balance}"
+  #Private
+  def _calculate_iva(self):
+    return  self.calculate_pay_month() * 0.16
+  #Private
+  def first_pay(self):
+    return  self.calculate_pay_month() + self._calculate_iva()
 
-
-# Variables # 
+# Variables = Users # 
 user_credit = Pay(2000, 0.08, 12, 6000)
-user_bank = Pay(4000, 0.15, 12, 10000)
-user_one = Pay(1500, 0.15, 18, 5000)
-
+user_bank = Pay(8000, 0.10, 12, 10000)
+user_one = Pay(15000, 0.15, 18, 5000)
+user_two = Pay(35000, 0.20, 24, 30000)
+user_three = Pay(50000, 0.25, 36, 10000)
 # Call Pay #
 if __name__ == "__main__": 
-  print("Pamela credit:", user_credit.money)
-  print("User Bank pay month:", user_credit.calculate_pay_month())
-  print("Pay Total User Bank:", user_bank._calculate_total_pay())
-  print("user Credit",user_credit._private_interest_rate())
+  user = f"Fred: Monto de su préstamo: {user_one.money:.2f} y pago total: {user_one._calculate_total_pay():.2f}, por mes pagaría: {user_one.calculate_pay_month():.2f} y los interéses del préstamo: {user_one._calculate_interest_rate():.2f}"
+  user_house = f"Alma: préstamo para su casa: {user_three.money:.2f} y pago total: {user_three._calculate_total_pay():.2f} por mes pagaría: {user_three.calculate_pay_month():.2f} y su primer pago se le suma iva: {user_three.first_pay():.2f} y los interéses del préstamo: {user_three._calculate_interest_rate():.2f}"
+  
+  #print(f"Monto que se prestó: {user_credit.money:.2f}")
+  #print(f"Pamela pay for month: {user_credit.calculate_pay_month():.2f}")
+  #print(f"Pamela pay Total: {user_credit._calculate_total_pay():.2f}")
+  #print(f"Interéses del préstamo: {user_credit._calculate_interest_rate():.2f}")
+  print(user)
+  print(user_house)
+
 
 
     
