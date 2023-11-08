bcd34002f697943e2cf6d1a344ccb39d380207f9
christiano0407 <placebo0704@gmail.com>
2023-11-08 12:39:42 -0600

    Add: Loop and def(functions) in POO Code

--- poo/poo_loops/poo_loop.py
+++ poo/poo_loops/poo_loop.py
@@ -7,8 +7,8 @@ class Borrow:
   def __init__(self, borrow, interest_rate, time_pay):
     self.borrow = borrow #Préstamo
     self.interest_rate = interest_rate #Tasa de Interés
-    self.balance = borrow #Saldo
     self.time_pay = time_pay #Meses sin interéses
+    self.balance = borrow #Saldo
 
   def pay_for_months(self):
     return self.borrow * self.interest_rate / 12 / (1 - (1 + self.interest_rate / 12) ** -self.time_pay)
@@ -17,28 +17,36 @@ class Borrow:
     return self.pay_for_months() * self.time_pay
   
   def calculate_balance(self, month):
-    if month == 0: 
+    if month == 0:
       return self.balance
     else:
-      return self.balance - self.pay_for_months()
+      self.balance -= self.pay_for_months() #Iterar
+      return self.balance
     
+#For loop & Def (1)
+def run_loop_emulation_pay(borrow, months):
+  for month in range(months):
+    pay_month = borrow.pay_for_months()
+    month_balance = borrow.calculate_balance(month)
+    print(f"***** For Pay: {month_balance:.2f}")
+    print(f"Month: {month}: Pay for month = {pay_month:.2f}, my balance = {month_balance:.2f}")
+
 
 #Variables
 user_borrow = Borrow(10000, 0.05, 24)
 client_borrow = Borrow(15000, 0.05, 36)
+user_brenda = Borrow(8000, 0.05, 6)
 
-#Loop
+#For Loop (2)
 for month in range(user_borrow.time_pay):
   month_pay = user_borrow.pay_for_months()
   balance_month = user_borrow.calculate_balance(month)
-  print(f"Month: {month}: Pay for month = {month_pay:.2f}, my balance = {balance_month:.2f}")
+  #print(f"Month: {month}: Pay for month = {month_pay:.2f}, my balance = {balance_month:.2f}")
+
 
 #Call Borrow
 if __name__ == "__main__":
-  burrows = user_borrow.borrow
-  #burrow_month = f"{user_borrow.pay_for_months():.2f}"
-  #user_client = f"This is I Pay for Month: {month_pay:.2f} and my balance: {balance_month:.2f}"
-
-  print(burrows)
-  #print(burrow_month)
-  #print(user_client)
+  #burrows = user_borrow.borrow
+  #print("Burrow:", burrows)
+  #run_loop_emulation_pay(client_borrow, client_borrow.time_pay)
+  run_loop_emulation_pay(user_brenda, user_brenda.time_pay)
