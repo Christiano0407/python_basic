a06185d93644fc47263c4295e1b82920902eea25
christiano0407 <placebo0704@gmail.com>
2023-11-09 12:41:25 -0600

    add new: Algorithm and POO

--- poo/poo_algorithm/poo_bubbleSort.py
+++ poo/poo_algorithm/poo_bubbleSort.py
@@ -1,13 +1,22 @@
 #### POO & Algorithm Bubble Sort ####
 
-def bubble_sort(lst):
+""" def bubble_sort(lst):
    #n = len(lst)
    for i in range(len(lst) - 1):
      for j in range(len(lst) -i -1):
        if lst[j] > lst[j + 1]:
          lst[j], lst[j + 1] = lst[j + 1] , lst[j]
 
-   return lst
+   return lst """
+
+def bubble_sort(list): 
+  n = len(list)
+  for i in range(n): 
+    for j in range(0, n - i -1):
+      if list[j] > list[j + 1]: 
+        list[j], list[j + 1] = list[j + 1], list[j]
+
+  return list
 
 
 class Borrow: 
@@ -37,6 +46,15 @@ class Borrow:
   def add_payments(self, payment): 
     self.payments.append(payment)
     return self.payments
+  
+  def order_payments(self): 
+    self.payments = bubble_sort(self.payments)
+    return self.payments
+
+  def pay_interest_rate(self): 
+    interests = self.payment_total() - self.borrow
+    return interests
+
 
   # Add Algorithm
 def payments_for_months(borrow, months): 
@@ -46,15 +64,31 @@ def payments_for_months(borrow, months):
     print(f"Pay for month: {pay_month:.2f}")
     print(f"My Balance for the Borrow: {pay_balance:.2f}")
 
+def payment_list(borrow, months):
+  for month in range(months):
+    #my_balance = borrow.balance_calculate( month)
+    payments_months = borrow.payment_month() #Pay Month
+    pay_month = borrow.add_payments(payments_months) # Add List Payments
+    formatted_payments = [f"{payment:.2f}" for payment in  pay_month] # Formatted
+    print(f"Payment (Month): {', '.join(formatted_payments)}") #Join Payments in List
+    #print(f"My Balance: {my_balance:.2f}")
+
   
 #Variable - Class
 user_plus = Borrow(10000, 0.08, 18, 25000)
 user_borrow = Borrow(25000, 0.10, 36, 50000)
+userOne_borrow = Borrow(5000, 0.4, 12, 10000)
 
 
 #Call Class #
 if __name__ == "__main__":
-  month = user_plus.payment_month()
-  total = user_plus.payment_total()
-  payments_for_months(user_borrow, user_borrow.time_pay)
-  print(f"**** {month:.2f} and {total:.2f}")
+  #month = user_plus.payment_month()
+  #total = user_plus.payment_total()
+  bubble_pay = userOne_borrow.order_payments()
+  formatted_bubble = [f"{pay:.2f}" for pay in bubble_pay]
+  print(f"Pay in Order: {formatted_bubble}")
+  payments_for_months(userOne_borrow, userOne_borrow.time_pay)
+  payment_list(userOne_borrow, userOne_borrow.time_pay)
+  #print(f"{userOne_borrow.payments:.2f}")
+  #print(f"Pay for Month: {month:.2f} and Total Pay: {total:.2f}")
+  #print(f"Interest_rate: {userOne_borrow.pay_interest_rate():.2f}")
