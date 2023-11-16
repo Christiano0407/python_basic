87cb62b4fb8a6126ddcfbe8bdcd4aff25dd0154c
christiano0407 <placebo0704@gmail.com>
2023-11-16 12:19:23 -0600

    New Web Server with FastAPI

--- backend/fast_api/main.py
+++ backend/fast_api/main.py
@@ -2,11 +2,40 @@
 #Fast API & Uvicorn
 ####
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+#HomePage
+@app.get('/', response_class=HTMLResponse)
+async def get_home():
+  html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1>Hello Dev! HTML & Python!</h1>
+            <h2>Python Backend </h2>
+            <p>Developer Web Content</p>
+        </body>
+    </html>
+    """
+  return HTMLResponse(content=html_content, status_code=200)
 
-@app.get('/')
-
-def get_list():
-  lst:[str] = list([2,4,7])
-  return f"My list: {lst}"
+#Content
+@app.get('/content', response_class=HTMLResponse)
+async def get_content():
+  html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1>Content Page</h1>
+            <p>Hello World! Python In Backend!!! Amazing Content</p>
+        </body>
+    </html>
+   """
+  return HTMLResponse(content=html_content, status_code=200)
