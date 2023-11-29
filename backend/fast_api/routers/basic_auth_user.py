7a7bcb053b7ddf40094b87576b6720658c922080
christiano0407 <placebo0704@gmail.com>
2023-11-29 09:08:34 -0600

    add: New authorization and authentication

--- backend/fast_api/routers/basic_auth_user.py
+++ backend/fast_api/routers/basic_auth_user.py
@@ -3,8 +3,9 @@
 #? OAuth2PasswordBearer : Se encarga de gestionar la autenticación (usuario e contraseña)
 #? OAuth2PasswordRequestForm: La forma en como se va a mandar nuestros criterios de autenticación.
 #? Token: TokeUrl => Se encarga de gestionar la validación. 
+#TODO: La declaración Annotated toma un tipo de dato existente y un objeto de anotación. El objeto de anotación puede ser cualquier objeto que proporcione el método __call__. El método __call__ se utiliza para obtener los metadatos asociados con el tipo de datos anotado.
 ########
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -13,6 +14,7 @@ app = FastAPI()
 #===oAuth2 ===
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+# ========= Crear un modelo de usuario ========
 #=== POO User === 
 class User(BaseModel):
   client_id: int
@@ -76,13 +78,50 @@ users_db = {
 """ app.get("/")
 async def get_user(): 
   return users_db """
-
+# Search User Token
 def search_user(username: str): 
   if username in users_db: 
     return UserDB(users_db[username])
   
+# Validation User Token (Criterio de dependencia)
+async def current_user(token: str = Depends(oauth2_scheme)):
+  '''
+  #Status 401 User Not Authorization but yes authentic user(Usuario sí está autenticado pero, no tiene autorización).
+  '''
+  user = search_user(token)
+  if not user:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED, 
+      detail="User not Authorization.", 
+      headers={"WWW-Authenticate": "Bearer"})
+
+  return user
 
 #POST (Mandar user & password)
 @app.post("/token")
 async def token(form: OAuth2PasswordRequestForm = Depends()):
+  '''
+  Depends es una característica integrada del marco FastAPI para definir dependencias en las rutas de la API. Las dependencias se utilizan para inyectar dependencias en las funciones de la API, lo que puede ser útil para una variedad de propósitos, como:
+  -Validación de datos de la solicitud.
+  -Obtención de acceso a servicios externos.
+  -Restringir el acceso a los recursos. 
+  En el contexto de OAuth 2.0, un token bearer es un tipo de token de acceso que se envía en el encabezado Authorization de una solicitud HTTP. El token bearer es simplemente una cadena de caracteres que no tiene ningún significado para el cliente que lo envía. El cliente es simplemente responsable de enviar el token al servidor. El servidor es responsable de validar el token y determinar a qué recursos tiene acceso el cliente.
+
+  El objeto JSON {"access_token", "token_type": "bearer"} es la respuesta de un servidor de autorización OAuth 2.0 que contiene el token bearer. La propiedad access_token contiene la cadena de caracteres del token bearer real. La propiedad token_type siempre es bearer para indicar que el token es un token bearer.
+
+  Los tokens bearer son una forma conveniente de implementar OAuth 2.0 porque son fáciles de usar y no requieren ningún estado del lado del cliente. Sin embargo, los tokens bearer también son menos seguros que otros tipos de tokens de acceso, como los tokens MAC, porque pueden ser fácilmente robados si el cliente está comprometido.
+  '''
   user_db = users_db.get(form.username)
+  if not user_db:
+    raise HTTPException(status_code=404, detail="This user is not correct.")
+  
+  user = search_user(form.username)
+  if not form.password == user.password:
+     raise HTTPException(status_code=404, detail="This password is not correct.")
+  
+  return {"access_token": user.username, "token_type": "bearer"}
+
+#GET
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+  return user
