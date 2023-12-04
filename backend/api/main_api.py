ceaca9b2eb24ffad01ab088b419e94c0289de48c
christiano0407 <placebo0704@gmail.com>
2023-12-04 12:19:08 -0600

    new methods and pydantic

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -60,7 +60,12 @@ async def read_home():
 async def get_movies():
   return {"movies": df.head(5).to_dict(orient="records")}
 
-#Path Parameters:
+
+@app.get("/movie/", status_code=200, tags=["movie"])
+async def get_all_movie(): 
+  return movies_api
+
+#==== Path Parameters:
 @app.get("/movie/{id}", status_code=200, tags=["movie"])
 async def get_movie(id: int):
   '''
@@ -89,7 +94,7 @@ async def get_year(year: int):
     raise HTTPException(status_code=500, detail=f"Server Error {str(e)}")
   
 
-#Query Parameters:
+#==== Query Parameters:
 @app.get("/movie/", status_code=200, tags=["movie"])
 async def query_movie(id: int, title: str, category: str):
   '''
@@ -141,3 +146,57 @@ async def create_movie(request: Request):
     movies_api.append(movie_data)
 
     return movie_data
+
+
+#===PUT
+#Function getMovieIndex
+def get_movie_index(movie_id):
+   '''
+   Se ha agregado la función get_movie_index para buscar el índice de una película en la lista según su ID.
+   Sí, enumerate es una función incorporada en Python que se utiliza para agregar un contador a un iterable y devolverlo como un objeto enumerado. Este objeto enumerado contiene pares de índices y elementos del iterable.
+   La función enumerate toma un iterable como argumento y devuelve un objeto enumerado
+   '''
+   for i, movie in enumerate(movies_api): 
+     if movie["id"] == movie_id:
+       return i
+   return None
+
+# = El método PUT generalmente se utiliza para actualizar un recurso existente =
+@app.put("/movie/{movie_id}", status_code=200, tags=["movie"])
+async def update_movie(movie_id: int, request: Request):
+    '''
+    Actualiza una película existente en la lista movies_api.
+    La función replace_movie utiliza await request.json() para obtener los nuevos datos de la película del cuerpo de la solicitud.
+    '''
+    movie_data = await request.json()
+    
+    # = Buscar el índice de la película en la lista = 
+    movie_index = get_movie_index(movie_id)
+
+    if movie_index is not None:
+      # = Actualizar los campos de la película con los nuevos datos =
+      movies_api[movie_index].update(movie_data)
+    else: 
+      raise HTMLResponse(status_code=404, detail=f"Movie with ID {movie_id} Not Exist.")
+
+
+#===DELETE
+@app.delete("/movie/{movie_id}", status_code=204, tags=["movie"])
+async def delete_movie(movie_id: int): 
+  '''
+   a) Elimina (delete) una película de la lista movies_api.
+   b) Si la película se encuentra, se utiliza movies_api.pop(movie_index) para eliminar la película de la lista. o usar: remove.
+   c) Se devuelve una respuesta con un código de estado 204 (No Content), que indica que la operación de eliminación fue exitosa y no hay contenido para devolver.
+  '''
+  
+  """ data_movie = await request.json() """
+
+  index_movie = get_movie_index(movie_id)
+
+  if index_movie is not None: 
+    delete_movie = movies_api.pop(index_movie)
+    return None
+  else: 
+    raise HTMLResponse(status_code=404, detail=f"Movie with this ID {movie_id} not found.")
+
+
