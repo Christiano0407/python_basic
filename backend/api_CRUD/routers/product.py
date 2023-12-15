2e8fdefce166d509c8752747e888fe2cf0fbf831
christiano0407 <placebo0704@gmail.com>
2023-12-15 12:57:07 -0600

    new api

--- backend/api_CRUD/routers/product.py
+++ backend/api_CRUD/routers/product.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, APIRouter, status, HTTPException
-import os
 import pandas as pd
 import numpy as np
+from pydantic import BaseModel, Field
+from typing import Union,Optional, List
+import os
+
 
 # === App & Router === 
 #app = FastAPI()
@@ -16,6 +19,19 @@ df = pd.read_csv(file_path)
 # Limpieza de valores NaN o infinitos
 df.replace([np.inf, -np.inf], np.nan, inplace=True)
 df.dropna(inplace=True)
+
+# ==== POO Instance Class ====
+class Product(BaseModel):
+  user_id: Union[int, None] = Field(..., gt=0)
+  product_id: Union[int, None] = Field(..., gt=0)
+  product_name: Union[str, None] = Field(description="Name of Product")
+  brand: Union[str, None] = Field(title="Name of Brand", description="Name of the Brand at product")
+  category: Union[str, None] = Field(min_length=1, max_length=15)
+  price: Union[int, None] = Field(gt=0, title="Prices Product", description="Prices of the Products")
+  rating: Union[int, None] = Field(min_length=0, max_length=10, title="Rating of Product", description="Value of Client for Product")
+  color: [str, None] = Field(title="Color of Product")
+  size: [str, Union] = Field(title="Size of Product", description="Size of Product")
+
 
 #=== API ROOT REST CRUD ===
 @router.get("/", status_code=status.HTTP_200_OK, tags=["products"])
