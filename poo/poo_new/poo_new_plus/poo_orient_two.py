c03020e28d6bd6d3aa9bd785cfd27ec6fd61b11e
christiano0407 <placebo0704@gmail.com>
2024-01-11 12:34:34 -0600

    add new Poo Oriented Object. Add Decorators Property

--- poo/poo_new/poo_new_plus/poo_orient_two.py
+++ poo/poo_new/poo_new_plus/poo_orient_two.py
@@ -17,10 +17,11 @@ class BankCard:
   nip_user = 745
   type_card = "Gold"
   # ==== Methods Of instance => Object ====
-  def __init__(self, card:str, bank:str, user:str):
+  def __init__(self, card:str, bank:str, user:str, num_card:int):
     self.__card = card
     self.__bank = bank
     self.__user = user
+    self.__num_card = num_card
 
   def __str__(self) -> str:
     return f"My bank is founder in: {self.country}"
@@ -45,11 +46,26 @@ class BankCard:
   def call_nip(self):
     print(f"Well, my type card is: {self.type_card}, my NIP: {self.nip_user} and my country {self.country}")
 
+  @property # ==> Permite: Solo lectura (read)
+  def get_num_private(self):
+     return self.__num_card
+  # > Setter (Change) - Access to Getters and Setters - <
+  @get_num_private.setter
+  def get_num_private(self, new_numPrivate):
+     if new_numPrivate == self.__num_card:
+        self.__num_card = new_numPrivate
+     else: 
+       print("Sorry! You cannot change your number of card")
+  
+
 #!==== === Execute === ====
 if __name__ == "__main__":
-  user_card = BankCard("BBVA", "BBVABank", "Chris")
+  user_card = BankCard("BBVA", "BBVABank", "Chris", 102345678)
   print(str(user_card))
   user_card.call_nip()#===Call to method @decorator (decorador)
   print(user_card.get_card())
   print(user_card.set_card("CityCard", "Citybanamex"))
   print("New:",user_card.get_card())
+  print("Property", user_card.get_num_private)
+  user_card.get_num_private = 102345678
+  """ print("Property change", user_card.num_private_card) """
