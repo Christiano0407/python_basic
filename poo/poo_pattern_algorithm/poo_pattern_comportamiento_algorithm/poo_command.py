86801e915e13faba88c3bfff85789b3cc674399d
christiano0407 <placebo0704@gmail.com>
2024-01-19 09:27:23 -0600

    add new patterns design behavior

---
+++ poo/poo_pattern_algorithm/poo_pattern_comportamiento_algorithm/poo_command.py
@@ -0,0 +1,4 @@
+######
+#TODO: === Design Pattern Behavior - Work Algorithms -: Command (Comando) ===
+#?: Command es un patrón de diseño de comportamiento que convierte una solicitud en un objeto independiente que contiene toda la información sobre la solicitud. Esta transformación te permite parametrizar los métodos con diferentes solicitudes, retrasar o poner en cola la ejecución de una solicitud y soportar operaciones que no se pueden realizar.
+#############
