5f50b8da34421b8ad55d918e0eaf653f5bbd1c30
christiano0407 <placebo0704@gmail.com>
2024-01-22 10:06:30 -0600

    add new pattern: Observer

---
+++ poo/poo_pattern_algorithm/poo_pattern_comportamiento_algorithm/poo_observer.py
@@ -0,0 +1,4 @@
+####
+#TODO: === Design Pattern Behavior - Work Algorithms - : Observer (Observar) ===
+#?: Observer es un patrón de diseño de comportamiento que te permite definir un mecanismo de suscripción para notificar a varios objetos sobre cualquier evento que le suceda al objeto que están observando.
+########
