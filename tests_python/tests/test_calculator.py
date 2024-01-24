5d2a19150d37aa00e01741f09fc79160e8a9af9e
christiano0407 <placebo0704@gmail.com>
2024-01-24 09:14:54 -0600

    add new test

--- tests_python/tests/test_calculator.py
+++ tests_python/tests/test_calculator.py
@@ -1,15 +1,15 @@
 ###
 #Todo: === Tests (02): Calculator ===
 ######
-from tests_python.calculator import add
+from calculator import add
 
 
 def tests_add(): 
-  assert add(5, 1) == 6
-  assert add(7, 4) == 11
-  assert add(-5, -5) == -10 
-  assert add(0, 0) == 0
-  assert add(1, 1) == 2  
+   assert add(5, 1) == 6
+   assert add(7, 4) == 11
+   assert add(-5, -5) == -10 
+   assert add(0, 0) == 0
+   assert add(1, 1) == 2  
   #assert add(-1, 2) == 0 # = Assertion Error = 
 
 
