29df046358aec7aa9dc69a98fd72d9fa58da32e1
christiano0407 <placebo0704@gmail.com>
2024-01-29 10:03:13 -0600

    Data base to Travel API

---
+++ backend/api_travel/main_trip.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+#*=== App ===
+app = FastAPI
+app.title  = "API REST & CRUD with FastAPI"
+app.version = "0.0.1"
+
+
+#* ==== Add Router ==== 
+
+
+#? ===== REST & CRUD =====
