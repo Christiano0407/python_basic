05a94efa323cf350512683514ca46a2e527b820e
christiano0407 <placebo0704@gmail.com>
2024-02-13 09:42:08 -0600

    add: Three positions

--- data_structure/array_cube.py
+++ data_structure/array_cube.py
@@ -1,3 +1,47 @@
 ######
-#! === Array: Tres Dimensiones (Cube) = (Methods and Behavior or Conduct) (Init & Limit) ===
+#! === Array: Tres Dimensiones (Cube: Axis Z - Eje Z - ) = (Methods and Behavior or Conduct) (Init & Limit) ===
 ######
+from create_array import Array
+from array_bidimensional import Grid
+
+
+class Cube:
+  def __init__(self, rows, columns, axis_z, array_value="None"): 
+    self.data = Array(axis_z)
+    for axis in range(axis_z):
+      self.data[axis] = Grid(rows, columns, array_value)
+
+  def get_height(self):
+    length = len(self.data)
+    return length
+  
+  def get_width(self):
+    # Accede a la primera capa (en z)
+    first_layer = self.data[0]
+    # Obtiene la primera fila (en y) de la primera capa
+    first_row = first_layer[0]
+    # Devuelve la longitud de la primera fila
+    width = first_row.__len__()
+    return width
+
+  
+  def get_axis_z(self): 
+    z_axis = len(self.data)
+    return z_axis
+  
+  def __getitem__(self, index):
+    return self.data[index]
+  
+  def __str__(self) -> str: 
+    result = ""
+    
+    for row in range(self.get_height()): 
+      for col in range(self.get_width()):
+        for axis_z in range(self.get_axis_z()): 
+          result += str(self.data[row][col][axis_z]) + " "
+
+          result += "\n"
+
+    return result
+
+
--- data_structure/array_result.py
+++ data_structure/array_result.py
@@ -1,21 +1,43 @@
 ######
-#! === Array ===
+#TODO === ==== Array (Structure Of Data) ==== ===
 ######
 from create_array import Array
 from array_bidimensional import Grid
+from array_cube import Cube
 
 
 menu = Array(10)
 twoDimension = Grid(3,3)
+threeDimension = Cube(2,2,2)
 
 
-# = Transform int(row & col) =
+# = 2D: Transform int(row & col) =
 def print_dimension(values):
   for row in range(values.get_height()):
     for col in range(values.get_width()):
       values[row][col] = row * col
       
   return values
+
+# = 3D : Transform int(row & col) =
+def print_three_dimension(value):
+  for row in range(value.get_height()):
+    for col in range(value.get_width()):
+      for z_axis in range(value.get_axis_z()):
+        value[row][col][z_axis] = row * col * z_axis
+      
+  return value
+
+def calculate_values_three_dimension(value):
+    new_value = Array(value.get_height())
+    for row in range(value.get_height()):
+        new_value[row] = Array(value.get_width())
+        for col in range(value.get_width()):
+            new_value[row][col] = Array(value.get_axis_z())
+            for z_axis in range(value.get_axis_z()):
+                new_value[row][col][z_axis] = row * col * z_axis
+    return new_value
+
 
 
 if __name__ == "__main__":
@@ -30,7 +52,7 @@ if __name__ == "__main__":
     print("Iter:", item)
   print("GetItem:", menu.__getitem__(1))
   print("SetItem:", menu.__setitem__(2,"Marvel"))
-  #? = Array two-dimensional =
+  #? == Array two-dimensional ==
   print("2D Two Dimension")
   print(twoDimension);
   print(print_dimension(twoDimension)); # Transform int(row & col)
@@ -39,4 +61,9 @@ if __name__ == "__main__":
   print(twoDimension.__getitem__(1))
   print(twoDimension.__str__())
   print(twoDimension.length())
+  #? === Array Three Dimensions ===
+  print("3D Dimension")
+  print(threeDimension)
+  print(print_three_dimension(threeDimension))
+  print(calculate_values_three_dimension(threeDimension))
 
